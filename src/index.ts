ac9e2cecf9c9736abad687cbb60928cf32ba0987
Alex Pinhasov <alexp.software@gmail.com>
2024-12-27 23:28:42 +0200

    add .js to index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 // Re-export from conversation.ts
-export * from './frontend/conversation';
+export * from './frontend/conversation.js';
 
 // Re-export from login.ts
-export * from './frontend/login';
+export * from './frontend/login.js';
 
 // Re-export from file-interface.ts
-export * from './frontend/file-interface';
+export * from './frontend/file-interface.js';
 
 // Re-export from Whatsapp/whatsapp.ts
-export * from './frontend/Whatsapp/whatsapp';
+export * from './frontend/Whatsapp/whatsapp.js';
 
 // Re-export from Whatsapp/template-creation-model.ts
-export * from './frontend/Whatsapp/template-creation-model';
+export * from './frontend/Whatsapp/template-creation-model.js';
 
-export * from './frontend/automations';
+export * from './frontend/automations.js';
