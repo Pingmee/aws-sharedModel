37c9018bda4b7a562a4c39c6d68dbfd888e2dc19
Alex Pinhasov <alexp.software@gmail.com>
2025-02-11 15:27:34 +0200

    Add google response model

--- src/frontend/login.ts
+++ src/frontend/login.ts
@@ -1,3 +1,15 @@
+export interface GoogleLoginResponse {
+  hd?: string
+  email: string
+  email_verified: boolean
+  name: string
+  picture: string
+  given_name: string
+  family_name: string
+  iat: string
+  exp: string
+}
+
 export enum LoginPlatform {
   email = 'email',
   google = 'google'
