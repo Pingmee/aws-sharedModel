7f9a264764e5ee3e411e4b4822420bb4a93e3b31
Alex Pinhasov <alexp.software@gmail.com>
2025-12-11 08:41:29 +0200

    Add storage types

---
+++ src/frontend/Storage.ts
@@ -0,0 +1,10 @@
+export type BusinessStorageTotal = {
+  associatedTo: string,
+  totalBytes: number
+}
+
+export type BusinessFileMetadata = {
+  associatedTo: string,
+  fileKey: string,
+  size: number,
+}
